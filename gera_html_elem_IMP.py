a192648b091121aa8c981c6090742adaf2069a68
Vinícius Balbino <vbalbinods@gmail.com>
2019-08-13 21:25:20 -0300

    Add botao_login

--- gera_html_elem_IMP.py
+++ gera_html_elem_IMP.py
@@ -55,6 +55,27 @@ def botao_subm_comprar(id_produto,qtd_produto,fam_fonte,tam_fonte,cor_texto,cor_
     "    </span>\n" + \
     "  </form>\n" + \
     "</span>"
+
+def botao_login():
+  """Função interna: Retorna fragmento de HTML5 que 
+  representa o botao de login"""
+  fam_fonte = "Courier"
+  tam_fonte = "18px"
+  cor_fundo = "#fff888"
+  return \
+    "<span style=\"\n" + \
+    "  display: inline-block;\n" + \
+    "  font-family:" + fam_fonte + ";\n" + \
+    "  font-size:" + tam_fonte + ";\n" + \
+    "  padding: 5px;\n" + \
+    "  background-color:" + cor_fundo + ";\n" + \
+    "  text-align: center;\n" + \
+    "\">\n" + \
+    "  <button\n" + \
+    "    type=\"button\"\n" + \
+    "    onclick=\"alert('" + "Login" + "')\"\n" + \
+    "  >" + "Login Button" + "</button>\n" + \
+    "</span>"
    
 def bloco_texto(texto,fam_fonte,tam_fonte,pad,halign,cor_texto,cor_fundo):
   return \
