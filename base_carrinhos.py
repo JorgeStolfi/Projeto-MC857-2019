1d223dbd5d675da46655a25149fdda98114cde76
Jorge Stolfi <stolfi@ic.unicamp.br>
2019-08-19 19:22:02 -0300

    Corrigindo nome do módulo para "base_carrinhos" (plural)

--- base_carrinhos.py
+++ base_carrinhos.py
@@ -1,26 +1,26 @@
-# Interface do módulo {base_carrinho}
+# Interface do módulo {base_carrinhos}
 
 # Interfaces importadas por esta interface:
 import carrinho
 import usuario
 
 # Implementaçao deste módulo:
-import base_carrinho_IMP
+import base_carrinhos_IMP
 
 # Funções exportadas por este módulo:
 
 def get_carrinho(usuario):
   """Dado um usuario, retornar o carrinho do usuario."""
-  return base_carrinho_IMP.get_carrinho()
+  return base_carrinhos_IMP.get_carrinho()
 
 def lista_todos_carrinhos(usuario):
   """Dado um usuario, retornar a lista com todos os carrinhos do usuario."""
-  return base_carrinho_IMP.lista_todos_carrinhos()
+  return base_carrinhos_IMP.lista_todos_carrinhos()
 
 def cria_novo_carrinho(usuario):
   """Dado um usuario, criar um novo carrinho para ele."""
-  return base_carrinho_IMP.cria_novo_carrinho()
+  return base_carrinhos_IMP.cria_novo_carrinho()
 
 def remove_carrinho(usuario, carrinho):
   """Dado um usuario e o carrinho desejado, remove de sua base de carrinhos."""
-  return base_carrinho_IMP.remove_carrinho()
+  return base_carrinhos_IMP.remove_carrinho()
