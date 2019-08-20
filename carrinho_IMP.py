61a8e9d14d1969e0353c8fdc3fee3cceb5ab431d
Vinícius Balbino <vbalbinods@gmail.com>
2019-08-20 20:37:57 -0300

    Arrumando Carrinho_IMP para arrumar Carrinho

--- carrinho_IMP.py
+++ carrinho_IMP.py
@@ -1,25 +1,34 @@
-# import produto
-# from produto import Produto
+""" import produto """
+""" from produto import Produto """
 
 class Carrinho_IMP:
     compras = dict()
 
     def cria(usr):
+    """ Retorna instancia do carrinho onde usuario colocará produtos """
         instance = Carrinho_IMP()
         instance.usr = usr
         return instance
 
     def pega_itens(self):
+    """ Retorna itens escolhidos pelo usuario """
         return compras.items()
 
     def calcula_total(self):
-        return
+    """ Calcula valor dos itens escolhidos pelo usuario """
+        total = 0
+        for elem in compras:
+            total += elem.valor*elem
+        return total
 
     def acrescenta_item(self, prod, qt):
+    """ Acrescenta uma quantidade de produtos ao carrinho """
         self.compras[prod] = qt
 
-    def troca_qtd( self, prod, qt):
+    def troca_qtd(self, prod, qt):
+    """ Muda a quantidade de produtos do carrinho """
         self.compras[prod] = qt
 
     def elimina_prod(self, prod):
+    """ Elimina produto do carrinho """
         self.compras.pop(prod)
