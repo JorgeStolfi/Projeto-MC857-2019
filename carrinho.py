29a28a3926d612540564f8b3af7faaa92c760b97
Senfernandes <senfernandes@gmail.com>
2019-08-20 19:33:52 -0300

    Corrige comentarios da classe carrinho.py

--- carrinho.py
+++ carrinho.py
@@ -3,27 +3,27 @@ from carrinho_IMP import *
 class Carrinho(Carrinho_IMP):
 
     def pega_itens():
-        #retorna uma tupla de itens e sua quantidade
+        """retorna uma tupla de itens e sua quantidade"""
         super().pega_itens()
 
     def calcula_total():
-        #retorna o preco total do carrinho
+        """retorna o preco total do carrinho"""
         super().calcula_total()
 
     def acrescenta_item(self, prod, qt):
-        #acrescenta um novo item no carrinho
+        """acrescenta um novo item no carrinho"""
         super().acrescenta_item(self, prod, qt)
 
     def troca_qtd( self, prod, qt):
-        #modifica a quantidade de um item
+        """modifica a quantidade de um item"""
         super().troca_qtd(self, prod, qt)
 
     def elimina_prod(self, prod):
-        #elimina um produto do carrinho
+        """elimina um produto do carrinho"""
         super().elimina_prod(self, prod)
 
     def cria(usr):
-        #cria uma nova instância com um usuário
+        """cria uma nova instância com um usuário"""
         instance = Carrinho()
         instance.usr = usr
         return instance
