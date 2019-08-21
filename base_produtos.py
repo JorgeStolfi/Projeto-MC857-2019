4beaab2a991250ec2dd62cfd299fb2eb405b9cbb
Pedro Barcha <pphbc@protonmail.com>
2019-08-20 22:22:43 -0300

    ra158338 atualizacao base_produtos

--- base_produtos.py
+++ base_produtos.py
@@ -2,33 +2,30 @@
 
 #Pedro Henrique Barcha Correia (PedroBarcha)
 #158338
-#Última Atualização: 13/08
+#Última Atualização: 20/08
 
-# Funções para busca de produtos
-# recebidos do usuário.  
+#Funções para busca de produtos
+#(requisições feitas pelo usuário).  
 
-# Estas funções devem devolver uma {List} dos produtos 
-# compatíveis com a pesquisa feita
+#Estas funções devolvem um ou mais objetos da classe {Produto}, 
+#compatíveis com a pesquisa feita
 
-# Implementação desta interface:
+#Implementação desta interface:
 import base_produtos_IMP
 
-def busca_produto(produto):
-  """Esta função checa quais campos de busca foram usados pelo usuário, chama as funções de busca correspondente e faz a junção das tabelas retornadas"""
-  return base_produtos_IMP.busca_produto_id(dados)
+def conecta():
+  """Abre conexao com a base de dados"""
+  return base_produtos_IMP.conecta()
 
-def busca_produto_nome(nome):
-  """Esta função processa uma busca por nome"""
-  return base_produtos_IMP.busca_produto_nome(dados)
+def desconecta(conexao_mysql):
+  """Fecha conexao com a base de dados"""
+  return base_produtos_IMP.desconecta(conexao_mysql)
 
-def busca_produto_tipo(tipo):
-  """Esta função processa uma busca por tipo de produto"""
-  return base_produtos_IMP.busca_produto_tipo(dados)
 
-def busca_produto_id(id):
-  """Esta função processa uma busca pelo id do produto"""
-  return base_produtos_IMP.busca_produto_id(dados)
+def busca_por_nome(nome, conexao_mysql):
+  """Devolve uma lista Python de objetos da classe {Produto}, consistindo de todos os produtos da base que tem a cadeia {nome} no seu nome, em ordem arbitrária. Se não houver nenhum produto que satisfaça essa busca, devolve {None}."""
+  return base_produtos_IMP.busca_por_nome(nome, conexao_mysql)
 
-def busca_produto_preco(preco_min, preco_max):
-  """Esta função processa uma busca por um intervalo de preco"""
-  return base_produtos_IMP.busca_produto_preco(dados)
+def busca_por_id(id, conexao_mysql):
+  """Devolve um único objeto da classe {Produto}, com o produto da base que tem a cadeia {id} em seu identificador. Se não houver nenhum produto que satisfaça essa busca, devolve {None}."""
+  return base_produtos_IMP.busca_por_id(id, conexao_mysql)
--- base_produtos_IMP.py
+++ base_produtos_IMP.py
@@ -2,20 +2,64 @@
 
 #Pedro Henrique Barcha Correia (PedroBarcha)
 #158338
-#Última Atualização: 13/08
+#Última Atualização: 20/08
 
-def busca_produto(produto):
-	print ("Produto encontrado!")
+#Implementação da base de produtos, conforme descrita na interface base_produtos.py
 
-def busca_produto_nome(nome):
-	print ("Nome: "+nome)
+###	IMPORTANTE ###
+#Conforme conversado não participarei das aulas dos dias 20 e 27, devido à intervenção cirúrgica, portanto fiz algumas suposições no código
+##################
 
-def busca_produto_tipo(tipo):
-	print ("Tipo: "+tipo)
+#Supondo que vamos usar pymysql
+import pymysql
 
-def busca_produto_id(id):
-	print ("Id:"+id)
+def conecta():
+	host = "127.0.0.1"
+	user = "root"
+	db = "mysql"
+
+	try:
+		#conecta à base de dados
+		conexao_mysql = pymysql.connect(host=host, unix_socket='/tmp/mysql.sock', user=user, passwd=None, db=db)
+
+		print("SUCESSO em estabelecer conexao com o banco de dados!")
+		return conexao_mysql
 	
-def busca_produto_preco(preco_min, preco_max):
-	print ("preco min:"+preco_min)
-	print ("preco max:"+preco_max)
+	except:
+		print("FALHA ao estabelecer conexao com o banco de dados!")
+		return None
+
+
+def desconecta(conexao_mysql):
+	conexao_mysql.cursor().close()
+	conexao_mysql.close()
+	
+
+def busca_por_nome(nome, conexao_mysql):
+	try:
+		#executa busca no banco de dados
+		cursor_mysql = conexao_mysql.cursor()
+		cursor_mysql.execute("SELECT nome FROM produto")
+		produtos_econtrados = cursor_mysql.fetchone()
+
+
+		print("Produtos encontrados: "+produtos_econtrados)
+		return produtos_econtrados
+
+	finally:
+		#fecha conexao com o banco de dados
+		desconecta(conexao_mysql)
+
+def busca_por_id(id, conexao_mysql):
+	try:
+		#executa busca no banco de dados
+		cursor_mysql = conexao_mysql.cursor()
+		cursor_mysql.execute("SELECT id FROM produto")
+		produtos_econtrados = cursor_mysql.fetchone()
+
+		print("Produtos encontrados: "+produtos_econtrados)
+		return produtos_econtrados
+	
+	finally:
+		#fecha conexao com o banco de dados
+		desconecta(conexao_mysql)
