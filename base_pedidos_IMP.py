626404930f0a25dd4aec7dea11c8dc75ffd4999a
Erico Faustino <ericoimf@gmail.com>
2019-08-20 21:29:31 -0300

    Ajeitar base_pedidos_IMP.py

--- base_pedidos_IMP.py
+++ base_pedidos_IMP.py
@@ -1,7 +1,43 @@
-#! /usr/bin/python3
+import base.py
 
-#Implementação teste das funçoes em base_pedidos_IMP
+#Retorna todos os pedidos do usuario
+def busca_por_usuario(usr):
+   query = "SELECT * FROM pedidos WHERE user_id = " + usr.id
+   usuario = base.executa_query(query)
+   return usuario
 
-def busca_pedidos_usuario(usuario):
-   print("Busca realizada" + usuario)
+#Busca o pedido pelo id
+def busca_por_id(pedido_id):
+   query = "SELECT * FROM pedidos WHERE pedido_id = " + pedido_id
+   pedido = base.executa_query(query)
+   return pedido
 
+#Busca todos os pedidos daquela data
+def busca_por_data(data):
+   query = "SELECT * FROM pedidos WHERE data = " + data
+   lista_pedidos = base.executa_query(query)
+   return lista_pedidos
+
+#Envia o pedido para o banco de dados
+def cria(ped):
+   query = "INSERT INTO pedidos"
+   sucesso = base.executa_query(query)
+   return sucesso
+
+#Recupera o pedido do banco de dados pelo id do pedido
+def recupera(ped):
+   query = "SELECT * FROM pedidos WHERE id = " + ped.id
+   pedido = base.executa_query(query)
+   return pedido
+
+#Atualiza o pedido no banco de dados
+def atualiza(ped):
+   query = "UPDATE pedidos"
+   sucesso = base.executa_query(query)
+   return sucesso
+
+#Deleta o pedido no banco de dados
+def deleta(ped):
+   query = "DELETE FROM pedidos WHERE pedido_id = " + ped
+   sucesso = base.executa_query(query)
+   return sucesso
