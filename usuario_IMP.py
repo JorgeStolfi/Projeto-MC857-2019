ee1439e531423a296c7e4d073de8d1a572019732
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-08-27 20:10:38 -0300

    Consertar usuario_IMP.py - falta verificar TODO base

--- usuario_IMP.py
+++ usuario_IMP.py
@@ -1,53 +1,78 @@
+import base_usuarios
 
 class Usuario_IMP:
+    id = ""
+    nome = ""
+    senha = ""
+    email = ""
+    CPF = ""
+    endereco = ""
+    telefone = ""
+
     def _init_(self,atrs):
-        self.nome = atrs.nome
-        self.sobrenome = atrs.sobrenome
-        self.nascDt = atrs.nascDt
-        self.senha = atrs.senha
-        self.email = atrs.email
-        self.CPF = atrs.CPF
-        self.endereco = atrs.endereco
-        self.telefone = atrs.telefone
-        
+        self.id = None
+        self.nome = atrs[nome]
+        self.senha = atrs[senha]
+        self.email = atrs[email]
+        self.CPF = atrs[CPF]
+        self.endereco = atrs[endereco]
+        self.telefone = atrs[telefone] 
+
     def obtem_atributos(self):
         return self
 
-def cria(atrs):
-    """Cria um novo usuário com um novo identificador único e os
-    atributos especificados pelo dicionário Python {atrs}."""
-    usuario = Usuario_IMP(atrs)
-    print("Bem vindo(a)" + usuario.nome + "a este e-commerce")
-    return usuario
+    def cria(self, atrs):
+        
+        cpf_atual = atrs[CPF]
+        email_atual = atrs[email]
 
-def muda_atributos(self,alts):
-    """Recebe um dicionário Python {alts} cujas chaves são um subconjunto
-    dos nomes de atributos do usuário, e troca os valores desses atributos 
-    pels valores correspondentes em {alts}."""
-    if alts.nome is not None:
-        self.nome = alts.nome
-    if alts.sobrenome is not None:
-        self.sobrenome = alts.sobrenome
-    if alts.nascDt is not None:
-        self.nascDt = alts.nascDt
-    if alts.senha is not None: 
-        self.senha = alts.senha
-    if alts.email is not None: 
-        self.email = alts.email
-    if alts.CPF is not None:
-        self.CPF = alts.CPF
-    if alts.endereco is not None:
-        self.endereco = alts.endereco
-    if alts.telefone is not None:
-        self.telefone = alts.telefone
+        # CPF ou email ja existe na base de usuarios
+        if ( base_usuarios.busca_por_CPF(cpf_atual) or \
+            base_usuarios.busca_por_email(email_atual) )
+            usr = None
+        else
+            copia_atrs = copy.deepcopy(atrs)
+            usr = Usuario_IMP(copia_atrs)
+            # TODO verificar primeiro argumento da chamada {bas}
+            id = base_usuarios.acrescenta(usr)
+            set_id(id)
+            
+        return usr
 
-def obtem_atributos(self):
-    """Retorna um dicionário Python com os atributos do usuário,
-    exceto identificador."""
-    return {self.nome, self.sobrenome, self.nascDt, self.email, self.CPF, self.endereco, self.telefone}
+    def muda_atributos(self, alts):
+        # CPF ou email ja existe na base de usuarios
+        if ( base_usuarios.busca_por_CPF(cpf_atual) or \
+            base_usuarios.busca_por_email(email_atual) )
+            return
 
-def obtem_identificador(self):
-    """Devolve uma cadeia consistindo das letras 'U-' e 8 algarismos decimais,
-    que identifica unicamente o usuário. Este identificador é 
-    atribuído na criação do usuário e não pode ser alterado."""
-    return self.login
+        if alts.nome is not None:
+            self.nome = alts.nome
+        if alts.nascDt is not None:
+            self.nascDt = alts.nascDt
+        if alts.senha is not None:
+            self.senha = alts.senha
+        if alts.email is not None:
+            self.email = alts.email
+        if alts.CPF is not None:
+            self.CPF = alts.CPF
+        if alts.endereco is not None:
+            self.endereco = alts.endereco
+        if alts.telefone is not None:
+            self.telefone = alts.telefone
+        
+         copia_atrs = copy.deepcopy(atrs)
+         # TODO Primeiro argumento Base ?
+         base_usuarios.atualiza(get_id(), self)
+
+    def obtem_atributos(self):
+        return {self.nome, self.sobrenome, self.nascDt, self.email, self.CPF, self.endereco, self.telefone}
+
+    def obtem_identificador(self):
+        return self.login
+
+    # Metodo "privado" para set id
+    def set_id(idt):
+        self.id = idt;
+    
+    def get_id():
+        return id
