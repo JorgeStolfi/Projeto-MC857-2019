102682521a21effca81227809b41099636829010
Vinícius Balbino <vbalbinods@gmail.com>
2019-08-27 19:47:15 -0300

    Create processa_comando_compra_IMP.py

---
+++ processa_comando_compra_IMP.py
@@ -0,0 +1,6 @@
+import processa_comando_compra
+
+def processa_compra(sessao, codigo_produto, quantidade):
+    pagina = '<html><header><title>Sessao' + sessao + '</title></header><body>Seu produto' + codigo_produto.id + 'de quantidade ' + quantidade ' 
+    esta sendo processado </body></html>'
+return pagina
