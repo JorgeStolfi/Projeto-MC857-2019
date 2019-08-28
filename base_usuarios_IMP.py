12c1f4dbe78e41202fa316daafdddf497b4a3405
guidumasperes <48294592+guidumasperes@users.noreply.github.com>
2019-08-27 21:28:28 -0300

    alteracao e implementacao de base_usuarios

--- base_usuarios_IMP.py
+++ base_usuarios_IMP.py
@@ -1,36 +1,28 @@
 #Essa classe é usada para simular o comportamento de uma referência de um banco de dados.
 import base
 
-def busca_por_indice(bas, ind):
-    '''
-    Devolve usuario numa busca por ind
-    '''
-    sql=("SElECT nome,sobrenome,nascDt,senha,email,cpf,endereco,telefone FROM usuarios WHERE ind = " + ind)
-    base.executa_query(sql)
-    console.log(f"Usuario {ind} devolvido")
-    return True
+def busca_por_indice(bas,ind):
+    sql="SELECT nome,senha,email,CPF,endereco,CEP,telefone FROM usuarios WHERE ind = " + ind
+    resultado=base.executa_comando(sql)
+    return resultado
 
 def busca_por_email(bas,em):
-    '''
-    Retorna usuario por email se existente.
-    '''
-    
-    sql=("SElECT nome,senha,email,cpf,endereco,telefone FROM usuarios WHERE email = " + em)
-    base.executa_comando(sql)
-    console.log(f"Usuario {em} devolvido")
-    return True
+    sql="SELECT nome,senha,CPF,endereco,CEP,telefone FROM usuarios WHERE email = " + em
+    resultado=base.executa_comando(sql)
+    return resultado
+
+def busca_por_CPF(bas,CPF):
+    sql="SELECT nome,senha,email,endereco,CEP,telefone FROM usuarios WHERE CPF = " + CPF
+    resultado=base.executa_comando(sql)
+    return resultado
 
 def acrescenta(bas,usr):
-    '''
-    Acrescenta um novo usuario
-    '''
-
-    console.log(f"Usuario {usr.name} created")
-    return 12345
+    sql="INSERT INTO usuario (nome,senha,email,CPF,endereco,CEP,telefone) VALUES (" + usr.nome + ", " + usr.senha + ", " + usr.email + ", "
+                                                                                    + usr.CPF + ", " + usr.endereco + ", " + usr.CEP + ", " + usr.telefone ");"
+    base.executa_comando(sql)
+    indice_inteiro=base.indice_inserido()
+    return indice_inteiro
 
 def atualiza(bas,ind,usr):
-    '''
-    Atualiza usuario com as propriedades de usr
-    '''
-    console.log(f"Usuario {usr.id} updated")
-    return 
+    sql="UPDATE usuario SET nome = " + usr.nome + ", senha = " + usr.senha + ", email = " + usr.email + ", CPF = " + usr.CPF + ", endereco = "
+                                     + usr.endereco + ", CEP = " + usr.CEP + ", telefone = " + usr.telefone " WHERE ind = " + ind
---
+++ testes/base_usuarios_TST.py
@@ -0,0 +1,19 @@
+#Testes referentes a base_usuarios.py
+import base_usuarios.py
+
+#Teste busca_por_indice
+resultado1 = busca_por_indice(bas, ind)
+
+#Teste busca_por_email
+resultado2 = busca_por_email(bas, em)
+
+#Teste busca_por_CPF
+resultado3 = busca_por_CPF(bas, CPF)
+
+#Teste acrescenta
+resultado4 = acrescenta(bas, usr)
+
+#Teste atualiza
+busca_por_indice(bas, ind, usr)
+
+#Como e necessario uma base de dados para executar os testes, ficam aqui apenas um esboço de como seriam.
