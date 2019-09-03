7ca98cb5778e5709d9fbb46a49ca3224597880b1
PedroBarcha <correia.barcha@gmail.com>
2019-09-03 20:40:18 -0300

    finalizacao de gera_html_botao*

--- gera_html_botao.py
+++ gera_html_botao.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Interface do módulo {gera_html_botao}.
 
 # As funções desta interface retornam cadeias de caracteres que são
@@ -8,6 +10,9 @@
 import gera_html_botao_IMP
 
 # As funções abaixo geram botões "<button>":
+def botao_simples(texto, cor_fundo):
+  """Função INTERNA para as demais funções de botões "<button>". Retorna HTML do botão "<button>" com texto e cor de fundo especificados."""
+  return gera_html_botao_IMP.botao_simples(texto, cor_fundo)
 
 def login():
   """Retorna fragmento de HTML5 que representa o botao de login"""
@@ -17,33 +22,33 @@ def logout():
   """Retorna fragmento de HTML5 que representa o botao de logout"""
   return gera_html_botao_IMP.logout()
 
-def cadastrar_usuario():
-  """retorna botão que vai ser incluído no menu geral de todas as páginas, e pede ao servidor uma página com o formulário para cadastrar um novo usuário."""
+def cadastra():
+  """Retorna botão que vai ser incluído no menu geral de todas as páginas, e pede ao servidor uma página com o formulário para cadastrar um novo usuário."""
   return gera_html_botao_IMP.cadastrar()
 
-def teste_de_popup(texto):
-  """Retorna HTML de um botão do menu
-  que mostra um popup com o {texto} dado."""
-  return gera_html_botao_IMP.teste_de_popup(texto)
 
 # As funções abaixo geram botões "<input type=submit>" para uso dentro de "<form>...</form>":
+def botao_submit(texto, cor_fundo):
+  """Função INTERNA para as demais funções de botões "<input type=submit>". Retorna HTML do botão <submit> com texto e cor de fundo especificados."""
+  return gera_html_botao_IMP.subm_botao_simples(texto, cor_fundo)
 
-def subm_comprar():
+def submit_compra():
   """Retorna um botão simples que é um fragmento HTML com o texto 'COMPRAR'."""
   return gera_html_botao_IMP.subm_comprar()
 
-def subm_login(login,senha):
+def submit_busca():
+  """Retorna HTML do botão <submit> para uso em formulario de busca."""
+  return gera_html_botao_IMP.subm_busca()
+  
+def submit_cadastra():
+  """Gera apenas o o botão "cadastrar" que vai estar dentro de um <form>...</form>, dentro da página de cadastrar novo usuário"""
+  return gera_html_botao_IMP.subm_cadastrar()
+
+def submit_login():
   """Retorna o botão para a submissão de login"""
   return gera_html_botao_IMP.subm_login()
 
-def subm_logout():
+def submit_logout():
   """Retorna o botão para a confirmação de logout"""
   return gera_html_botao_IMP.subm_logout()
-  
-def subm_cadastrar(texto):
-    """Gera apenas o o botão "cadastrar" que vai estar dentro de um <form>...</form>, dentro da página de cadastrar novo usuário"""
-    return gera_html_botao_IMP.subm_cadastrar(texto)
 
-def subm_busca():
-  """Retorna HTML do botão <submit> para uso em formulario de busca."""
-  return gera_html_botao_IMP.subm_busca()
