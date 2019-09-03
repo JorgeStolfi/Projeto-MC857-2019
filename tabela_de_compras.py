921165fda30397f903aba19d0036c6e1c6094230
João Victor Fernandes <lfvictorf@gmail.com>
2019-09-03 20:37:12 -0300

    Implementacao da interface tabela_de_compras

--- tabela_de_compras.py
+++ tabela_de_compras.py
@@ -3,36 +3,36 @@
 # Cada linha desta tabela representa um pedido de compras (ou um
 # carrinho de compras) de um cliente. Tem a maioria dos campos de
 # objeto de classe {ObjCompra}, menos os itens da compra
-# (que estão numa tabela separada, {tabela_de_itens_de_compras.py}. 
-# 
+# (que estão numa tabela separada, {tabela_de_itens_de_compras.py}.
+#
 # Cada linha tem um índice inteiro (chave primária) que é atribuído
 # quando a linha é criada. As funções e métodos desta base manuseiam o
 # índice na forma de um identificador de compra "C-{NNNNNNNN}".
-# 
+#
 # Especificamente, cada linha tem os seguintes campos de informação
 # (colunas da tabela):
-#   
+#
 #   'id_compra' {str} O identificador do pedido de compra, "C-{NNNNNNNN}".
 #   'id_usuario' {str} O identificador do cliente da compra, "U-{NNNNNNNN}".
 #   'status' {str} O status da compra: 'aberto', 'pagando', etc.
-#   
+#
 # Outros campos (como datas, endereço de entrega, forma de pagamento, etc.)
-# serão acrescentados oportunamente.  Todos os campos, menos 
+# serão acrescentados oportunamente.  Todos os campos, menos
 # 'id_compra' e 'id_usuario', podem ser alterados.
-# 
+#
 # Nas funções abaixo, uma linha da tabela é representada na memória
 # por um dicionário Python {atrs} cujas chaves e valores
 # são os campos da linha.
-# 
+#
 # Nas funções abaixo, o parametro {bas} é um objeto da classe {Base_SQL}
-# que representa a base de dados da loja.  
+# que representa a base de dados da loja.
 
 # Implementação desta interface:
 import tabela_de_compras_IMP
 
 def cria_tabela(bas):
   """Cria a tabela de compras na base de dados {bas}.
-  Esta função deve ser chamada apenas uma vez, quando o 
+  Esta função deve ser chamada apenas uma vez, quando o
   servidor é iniciado."""
   tabela_de_compras_IMP.cria_tabela(bas)
 
@@ -43,18 +43,45 @@ def acrescenta(bas,atrs):
   O dicionário não deve conter o campo 'id_compra'."""
   return tabela_de_compras_IMP.acrescenta(bas,atrs)
 
+ def recupera(bas,cpr):
+    """Devolve um dicionário {atrs} com o conteúdo da linha de identificador {cpr.id} da tabela de compras da base {bas}.
+    O dicionário não inclui o campo 'id'.
+    """
+    return tabela_de_compras_IMP.recupera(bas, cpr)
+
+def atualiza(bas,cpr):
+   """Atuliza uma compra com id {cpr.id} na tabela de compras da base {bas}. E retorna status de Sucesso ou falha.
+   """
+   return tabela_de_compras_IMP.atualiza(bas, cpr)
+
+def deleta(bas,cpr):
+   """Deleta uma compra com id {cpr.id} na tabela de compras da base {bas}. E retorna status de Sucesso ou falha.
+   """
+   return tabela_de_compras_IMP.deleta(bas, cpr)
+
 def busca_por_identificador(bas,id_compra):
-  """Devolve um dicionário {atrs} com o conteúdo da linha 
+  """Devolve um dicionário {atrs} com o conteúdo da linha
   de identificador {id_compra} da tabela de compras da base {bas}.
   O dicionário não inclui o campo 'id_compra'."""
   return tabela_de_compras_IMP.busca_por_identificador(bas,id)
 
+def busca_por_data(bas,data):
+  """Devolve um array de dicionários {atrs} com os conteúdo da linhas
+  de data {data} da tabela de compras da base {bas}.
+  O dicionário não inclui o campo 'data'."""
+  return tabela_de_compras_IMP.busca_por_data(bas,data)
+
 def atualiza(bas,id_compra,atrs):
   """Modifica alguns campos de uma linha da tabela de compras da base {bas}.
-  A linha é especificada pelo seu identificador {id_compra}.  Não devolve 
+  A linha é especificada pelo seu identificador {id_compra}.  Não devolve
   nenhum resultado.
-  
+
   O parâmetro {atrs} deve ser um dicionário cujas chaves são um
   subconjunto dos nomes dos campos da linha (excluindo 'id_compra').
   Os valores desses campos na tabela são substituídos pelos valores em {atrs}."""
   tabela_de_compras_IMP.atualiza(bas,id_compra,atrs)
+
+def busca_por_usuario(bas,usr):
+    """Devolve uma lista de compras (array de dicionários python) nas quais o comprador é representado por {usr}
+    """
+    return tabela_de_compras_IMP.busca_por_usuario(bas, usr)
