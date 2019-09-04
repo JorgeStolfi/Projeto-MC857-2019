83bccc8d9ac92915dce6727d8b8d6b5e2a4c71c6
Lucas Keiji Takeuti <ra158160@students.ic.unicamp.br>
2019-09-03 21:10:01 -0300

    Implementação basica de criacao e busca, teste

--- tabela_de_itens_de_compras_IMP.py
+++ tabela_de_itens_de_compras_IMP.py
@@ -1,32 +1,41 @@
 import base_sql
+import identificador
 
-def cria_tabela(bas,chaves):
-  cmd = "CREATE TABLE compras (" + \
-    "indice int(8) NOT NULL AUTO_INCREMENT,"
-    
-  for ch in chaves:
-    cmd = cmd + ch + " varchar(,"
+def cria_tabela(bas):
+  cmd = "CREATE TABLE itens_compras (" + \
+    "id_item int(8) NOT NULL AUTO_INCREMENT," +\
+    "id_compra int(8) NOT NULL AUTO_INCREMENT," +\
+    "id_produto int(8) NOT NULL AUTO_INCREMENT," +\
+    "qt float NOT NULL AUTO_INCREMENT," +\
+    "preco float NOT NULL AUTO_INCREMENT," +\
+    "PRIMARY KEY id_item"
+  bas.executa_comando(cmd)
+  
+def acrescenta(bas,atrs):
+   campos="id_compra,id_produto,qt,preco"
+   valores = \
+   atrs["id_compra"] + ", " + \
+   atrs["id_produto"] + ", " + \
+   atrs["qt"] + ", " + \
+   atrs["preco"]
+       
+   cmd = "INSERT INTO itens_compras(" + campos + ") VALUES (" + valores + ");"
+   sucesso = bas.executa_comando(cmd)
+   ind = bas.indice_inserido() 
+   return   identificador.de_indice("I",ind)
 
-def busca_por_usuario(bas,usr,abr):
-   cmd = "SELECT * FROM compras WHERE user_id = " + usr.id
-   usr = bas.executa_comando(cmd)
-   return usr
 
-def busca_por_id(bas,compra_id):
-   cmd = "SELECT * FROM compras WHERE compra_id = " + compra_id
+def busca_por_compra(bas,id_compra):
+   cmd = "SELECT id_item FROM itens_compras WHERE id_compra = " + str(id_compra) + ";"
    cpr = bas.executa_comando(cmd)
    return cpr
 
-def busca_por_data(bas,data):
-   cmd = "SELECT * FROM compras WHERE data = " + data
-   lista_compras = bas.executa_comando(cmd)
-   return lista_compras
+def busca_por_produto(bas,id_produto):
+   cmd = "SELECT id_item FROM compras WHERE id_produto = " + id_produto + ";"
+   cpr = bas.executa_comando(cmd)
+   return cpr
 
-def acrescenta(bas,cpr):
-   cmd = "INSERT INTO compras"
-   sucesso = bas.executa_comando(cmd)
-   ind = bas.indice_inserido()
-   return ind
+
 
 def recupera(bas,cpr):
    cmd = "SELECT * FROM compras WHERE id = " + cpr.id
--- testes/tabela_de_itens_de_compras_TST.py
+++ testes/tabela_de_itens_de_compras_TST.py
@@ -1,8 +1,37 @@
-#! /usr/in/python3
+#! /usr/bin/python3
 
 import tabela_de_sessoes
 import sessao; from sessao import ObjSessao
 import usuario; from usuario import ObjUsuario
+
+import tabela_de_itens_de_compras as itens
+import base_sql
+import identificador
+import sys
+
+# Conexao com a base de dados:
+bas = base_sql.conecta("ITEM",None,None)
+
+sys.stderr.write("Testando tabela_de_itens_de_compras ...\n")
+
+#cria a a tabela_de_itens_de_compras
+itens.cria_tabela(bas)
+
+#Teste acrescenta
+atrs = {
+  "id_compra" : "00000001",
+  "id_produto" : "00000002",
+  "qt" : "32",
+  "preco" : "3.50"
+}
+#acrescenta linha
+itens.acrescenta(bas,atrs)
+id_compra = int("00000001")
+lista = itens.busca_por_compra(bas,id_compra)
+for i in lista:
+  sys.stderr.write(str(i[0]))
+
+
 
 def valida_busca_por_indice(bas):
     ses = sessao.cria(bas)
@@ -25,7 +54,7 @@ def valida_acrescenta(bas):
 def valida_atualiza(bas):
     ult_id = bas.indice_inserido()
     ult_ses = tabela_de_sessoes.busca_por_indice(ult_id)
-    aberta = 
+
     if ult_ses.aberta() == True:
         ult_ses.logout()
     else:
