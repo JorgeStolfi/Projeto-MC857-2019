0ebe82c51c198851a7fcf92a39b34ad787bc2901
PedroBarcha <correia.barcha@gmail.com>
2019-09-10 20:59:24 -0300

    interface sessao.py

--- sessao.py
+++ sessao.py
@@ -6,10 +6,15 @@
 
 # Interfaces importadas por esta interface:
 import usuario; from usuario import ObjUsuario
-import base_sql; from base_sql import Base_SQL
 
 # Implementaçao deste módulo:
 import sessao_IMP; from sessao_IMP import ObjSessao_IMP
+
+def inicializa():
+  """Inicializa o modulo, criando a tabela de sessões na base de dados.
+  Deve ser chamada apenas uma vez no ínicio da execução do servidor.
+  Não retorna nenhum valor."""
+  usuario_IMP.inicializa()
 
 class ObjSessao(ObjSessao_IMP):
   """Um objeto desta classe representa uma sessao de acesso ao
@@ -22,125 +27,72 @@ class ObjSessao(ObjSessao_IMP):
   
   Cada sessao pertence a um unico usuario, mas cada
   usuário pode ter várias sessoes abertas ao mesmo tempo. A sessao é criada
-  e "aberta" quando o usuario faz login, e e "fechada" no logout. """
+  e "aberta" quando o usuario faz login, e e "fechada" no logout.
 
-  def obtem_identificador(self):
-    """Devolve uma cadeia no formato 'S-{NNNNNNNN}', onde 
-    {NNNNNNNN} e um numero de 8 algarismos 
-    que identifica unicamente a sessao. Este identificador e 
-    atribuido na criacao da sessao e nao pode ser alterado."""
-    return ObjSessao_IMP.obtem_identificador(self)
-
-  def obtem_atributos(self):
-    """Retorna um dicionário Python que é uma cópia dos atributos da sessão,
-    exceto identificador."""
-    return ObjSessao_IMP.obtem_atributos(self)
-    
-  def muda_atributos(self, alts):
-    """Recebe um dicionário Python {alts} cujas chaves são um subconjunto
-    dos nomes de atributos da sessão (exceto o identificador), e troca os 
-    valores desses atributos pelos valores correspondentes em {alts}.  
-    Também altera esses campos na base de dados.
-    Em caso de sucesso, retorna o próprio objeto."""
-    return ObjSessao_IMP.muda_atributos(self, alts)
-
-  def obtem_usuario(self):
-    """Retorna o objeto da classe {ObjUsuario} correspondente ao usuario que
-    fez login na sessao.  Equivale a {self.obtem_atributos()['usr']}. """
-    return ObjSessao_IMP.obtem_usuario(self)
-
-  def aberta(self):
-    """Retorna o estado da sessão: {True} se a sessao ainda esta aberta, 
-    {False} se o usuário deu logout.  Equivale a {self.obtem_atributos()['aberta']}."""
-    return ObjSessao_IMP.aberta(self)
-    
-  def logout(self):
-    """Registra o logout do usuário na sessão, mudando o atributo 'aberta'
-    permanentemente para {False}. Também altera esse campo na base de dados.
-    Em caso de sucesso, retorna o próprio objeto."""
-    return ObjSessao_IMP.logout(self)
-    
+   Cada sessão do sistema -- aberta ou fechada -- é representada por uma linha na tabela "sessoes" da base SQL em
+  disco. Apenas algumas dessas linhas são representadas também na memória por objetos
+  da classe {ObjSessao}. 
+  
+  Cada linha da tabela tem um índice inteiro (chave primária) distinto, que é atribuído
+  quando a linha é criada.  Neste sistema, esse índice é manipulado na forma de 
+  um identificador de sessão, uma string da forma "S-{NNNNNNNN}"
+  onde {NNNNNNNN} é o índice formatado em 8 algarismos.
+  
+  Além disso, cada linha tem uma coluna da tabela (um campo) para cada um dos
+  atributos da sessão (menos o identificador), como definido por {sessão.campos()}.
+  """
 # Construtor da classe:
 
 def cria(usr):
   """Cria um novo objeto da classe {ObjSessao}, associada ao usuário {usr},
   inicialmente aberta.  Também acrescenta a sessão à base de dados.  Em caso de
-  sucesso, retorna o objeto."""
+  sucesso, retorna o objeto.
+  Atribui um identificador único à sessão, derivado do seu índice na tabela.
+  Retorna o objeto criado."""
   return sessao_IMP.cria(usr)
 
-# Descrição dos campos:
+def obtem_identificador(sessao):
+	"""Devolve o identificador 'S-{NNNNNNNN}' da sessão."""
+    return sessao_IMP.obtem_identificador(sessao)
 
+def obtem_usuario(sessao):
+    """Retorna o objeto da classe {ObjUsuario} correspondente ao usuario que
+    fez login na sessao.  Equivale a {self.obtem_atributos()['usr']}. """
+    return sessao_IMP.obtem_usuario(sessao)
+
+def busca_por_identificador(id_sessao):
+    """Devolve um dicionário {atrs} com o conteúdo da linha 
+    de identificador {id_sessao} da tabela de sessões da base {bas}.
+    O dicionário não inclui o campo 'id_sessao'."""
+    return sessao_IMP.busca_por_identificador(id_sessao)
+
+def aberta(sessao):
+    """Retorna o estado da sessão: {True} se a sessao ainda esta aberta, 
+    {False} se o usuário deu logout.  Equivale a {self.obtem_atributos()['aberta']}."""
+    return sessao_IMP.aberta(sessao)
+
+def obtem_atributos(sessao):
+    """Retorna um dicionário Python que é uma cópia dos atributos da sessão,
+    exceto identificador."""
+    return sessao_IMP.obtem_atributos(sessao)
+    
+def muda_atributos(sessao, alts):
+    """Recebe um dicionário Python {alts} cujas chaves são um subconjunto
+    dos nomes de atributos da sessão (exceto o identificador), e troca os 
+    valores desses atributos pelos valores correspondentes em {alts}.  
+    Também altera esses campos na base de dados.
+    """
+    sessao_IMP.muda_atributos(sessao, alts)
+    
+def logout(sessao):
+    """Registra o logout do usuário na sessão, mudando o atributo 'aberta'
+    permanentemente para {False}. Também altera esse campo na base de dados.
+    Em caso de sucesso, retorna o próprio objeto."""
+    return sessao_IMP.logout(sessao)
+    
 def campos():
   """Retorna uma seqüência de tuplas que descrevem os nomes e propriedades
   dos atributos de um {ObjSessao}, menos o identificador.  O resultado é adequado 
   para o parâmetro {cols} das funções do módulo {tabela_generica}."""
   return sessao_IMP.campos()
-# ======================================================================
-# Funções para acesso à base de dados das sessões.
 
-# Implementação desta interface:
-import tabela_de_sessoes_IMP
-from tabela_de_sessoes_IMP import Obj_Tabela_De_Sessoes_IMP
-
-class Obj_Tabela_De_Sessoes(Obj_Tabela_De_Sessoes_IMP):
-  # Um objeto (instância) desta classe representa a tabela de sessoes
-  # da base de dados. Deve haver uma única instância 
-  # no servidor.
-  # 
-  # Cada linha desta tabela representa uma sessão de login, ativa ou 
-  # encerrada.  Os campos são basicamente os mesmos de um objeto de 
-  # classe {ObjSessao}. 
-  # 
-  # Cada linha tem um índice inteiro (chave primária) que é atribuído
-  # quando a linha é criada. As funções desta interface manuseiam o
-  # índice na forma de um identificador de sessao "S-{NNNNNNNN}".
-  # 
-  # Especificamente, cada linha tem os seguintes campos de informação
-  # (colunas da tabela):
-  #   
-  #   'id_sessao' {str} O identificador do sessao, "S-{NNNNNNNN}".
-  #   'id_usuario' {str} - O identificador do usuário, "U-{NNNNNNNN}".
-  #   'aberta' {bool} - {True} se a sessão está ativa, {False} se o cliente deu logout.
-  #   
-  # Outros campos (como peso, volume, estoque, imagens, palvras-chave, etc.)
-  # serão acrescentados oportunamente.
-  # 
-  # O identificador 'id_sessao' de cada linha é atribuído sequencialmente
-  # quando a linha é criada.  Todos os campos, exceto 'id_sessao', e 'id_usuario',
-  # podem ser alterados.
-  # 
-  # Nas funções abaixo, uma linha da tabela é representada na memória
-  # por um dicionário Python {atrs} cujas chaves e valores
-  # são os campos da linha.
-
-  # Nas funções abaixo, o parametro {bas} é um objeto da classe {Base_SQL}
-  # que representa a base de dados da loja.
-
-  def acrescenta(self,atrs):
-    """Acrescenta uma nova linha na tabela de sessões
-    da base {bas}, cujo conteúdo é o dicionário {atrs},
-    e devolve o identificador "S-{NNNNNNNN}" da mesma.
-    O dicionário não deve conter o campo 'id_sessao'."""
-    return Obj_Tabela_De_Sessoes_IMP.acrescenta(self,ses)
-
-  def busca_por_identificador(self,id_sessao):
-    """Devolve um dicionário {atrs} com o conteúdo da linha 
-    de identificador {id_sessao} da tabela de sessões da base {bas}.
-    O dicionário não inclui o campo 'id_sessao'."""
-    return Obj_Tabela_De_Sessoes_IMP.busca_por_identificador(self,id_sessao)
-
-  def atualiza(self,id_sessao,atrs):
-    """Modifica alguns campos de uma linha da tabela de sessões da base {bas}.
-    A linha é especificada pelo seu identificador {id_sessao}. 
-    Não devolve nenhum resultado.
-
-    O parâmetro {atrs} deve ser um dicionário cujas chaves são um
-    subconjunto dos nomes dos campos da linha (excluindo 'id_sessao'). 
-    Os valores desses campos na tabela são substituídos pelos valores em {atrs}."""
-    Obj_Tabela_De_Sessoes_IMP.atualiza(self,id_sessao,atrs)
-
-def cria_tabela(bas):
-  """Cria a tabela de sessões na base de dados {bas}.
-  Esta função deve ser chamada apenas uma vez, quando o 
-  servidor é iniciado."""
-  return tabela_de_sessoes_IMP.cria_tabela(bas)
