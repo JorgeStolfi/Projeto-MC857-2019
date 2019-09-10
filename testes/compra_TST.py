e7ffd6aae485aa2ddb26ba4f8f70489a97c96c38
Lucas Keiji Takeuti <ra158160@students.ic.unicamp.br>
2019-09-10 20:22:37 -0300

    Modificaçoes em compra_TST nos padroes de usuario_TST

--- testes/compra_TST.py
+++ testes/compra_TST.py
@@ -8,186 +8,98 @@ import base_sql
 import tabela_de_compras
 import identificador
 
-
+#inicializa banco de dados
 sys.stderr.write("Conectando com base de dados...\n")
 bas = base_sql.conecta("DB/MC857",None,None)
 
-sys.stderr.write("Criando tabela de compras...\n")
-res = tabela_de_compras.cria_tabela(bas)
+#inicializa modulo compra
+compra.inicializa()
+
+colunas = compra.campos()
+
+#limpa a tabela de compras
+sys.stderr.write("Limpando tabela de compras...\n")
+res = tabela_generica.limpa_tabela("compras", colunas)
 sys.stderr.write("Resultado = " + str(res) + "\n")
 
-usr_atrs = {
-    "nome": "Alberto Nogueira",
-    "senha": "1234",
-    "email": "alberto@nogueira.com",
-    "CPF": "123.456.789-10",
-    "endereço": "Av. Dr. Romeu Tórtima 992",
-    "CEP": "13083-440",
-    "telefone": "+55(19)9999-9999"
-}
-
-# usr = usuario.cria(bas,usr_atrs)
-""" Ignorando usuario a fim de teste """
-usr = None
-cpr = compra.cria(bas,usr)
-
-sys.stderr.write(str(cpr.obtem_identificador()) + "\n")
-sys.stderr.write(str(cpr.obtem_usuario()) + "\n")
-
-sys.stderr.write(str(cpr.obtem_status()) + "\n")
-sys.stderr.write(str(cpr.lista_itens()) + "\n")
-sys.stderr.write(str(cpr.calcula_total()) + "\n")
-
-prod_atrs = {
-  'descr_curta': "Escovador de ouriço",
-  'descr_media': "Escovador para ouriços ou porcos-espinho portátil em aço inox e marfim orgânico, com haste elongável, cabo de força, 20 acessórios, e valise.",
-  'descr_longa': "Fabricante: Ouricex SA\nOrigem: Cochinchina\nModelo: EO-22\nTensão: 110-230 V\nPotência: 1500 W\nDimensões: 300 x 200 x 3000 mm",
-  'preco': 120.00,
-  'unidade': '1 aparelho' }
 
 
-# prod = produto.cria(bas,prod_atrs)
-""" Criando objeto produto"""
-prod = ObjProduto(prod_atrs)      
-
-cpr.acrescenta_item(prod,3)
-
-sys.stderr.write(str(cpr.lista_itens()) + "\n")
-
-cpr.troca_qtd(prod, 4)
-
-sys.stderr.write(str(cpr.lista_itens()) + "\n")
-
-cpr.elimina_prod(prod)
-
-sys.stderr.write(str(cpr.lista_itens()) + "\n")
-# ======================================================================
-#! /usr/bin/python3
-
-import tabela_de_compras as tb_cpr
-import tabela_de_usuarios as tb_usr
-import compra; from compra import ObjCompra
-import usuario; from usuario import ObjUsuario
-
-sys.stderr.write("Conectando com base de dados...\n")
-bas = base_sql.conecta("DB/MC857",None,None)
-
-sys.stderr.write("Criando tabela de usuarios...\n")
-res = tb_usr.cria_tabela(bas)
-sys.stderr.write("Resultado = " + str(res) + "\n")
-
-sys.stderr.write("Criando tabela de compras...\n")
-res = tb_cpr.cria_tabela(bas)
-sys.stderr.write("Resultado = " + str(res) + "\n")
-
-def valida_busca_por_identificador(bas):
-    cpr = compra.cria(bas)
-    ind = cpr.acrescenta(bas,cpr)
-    res = cpr.busca_por_identificador(ind)
-    if cpr != res:
-        print("O resultado esperado era " + str(cpr) + " mas foi retornado " + str(res))
-    else:
-        print("Foi retornado o resultado esperado")
-
-def valida_acrescenta(bas):
-    ultimo = bas.indice_inserido()
-    cpr = compra.cria(bas)
-    novo = cpr.acrescenta(cpr)
-    if novo <= ultimo:
-        print("O ID inserido esta incorreto")
-    else:
-        print("A entrada inserida está correta")
-
-def valida_atualiza(bas):
-    ult_id = bas.indice_inserido()
-    ult_cpr = cpr.busca_por_identificador(ult_id)
-    if ult_cpr.aberta() == True:
-        ult_cpr.logout()
-    else:
-        usr = usuario.cria(bas,atrs)
-        ult_cpr.login(usr)
-    nova_aberta = ult_cpr.aberta()
-    bas.atualiza(ult_cpr)
-
-    res = bas.busca_por_identificador(ult_id)
-    if res.aberta() != nova_aberta:
-        print("O resultado esta errado")
-    else:
-        print("O resultado está correto")
+# ----------------------------------------------------------------------
+def mostra_compra(rotulo,cpr,id,atrs):
+  """Imprime compra {cpr} e compara seus atributos com {id,atrs}."""
+  sys.stderr.write(rotulo + " = \n")
+  if cpr == None:
+    sys.stderr.write("None\n")
+  elif type(cpr) is ObjCompra_IMP:
+    sys.stderr.write("  id = " + str(compra.obtem_identificador(cpr)) + "\n")
+    sys.stderr.write("  atrs = " + str(cpr.obtem_atributos(cpr)) + "\n")
+    if atrs != None:
+      id_confere = (compra.obtem_identificador(cpr) == id)
+      atrs_conferem = (cpr.obtem_atributos(cpr) == atrs)
+      sys.stderr.write("  CONFERE: " + str(id_confere) + ", " + str(atrs_conferem) + "\n")
+ 
+def testa_cria_compra(rotulo,id,atrs):
+  """Testa criação de usuário com atributos com {atrs}. Retorna o usuário."""
+  cpr = compra.cria(atrs)
+  mostra_compra(rotulo,usr,id,atrs)
+  return cpr
+ 
 
 
-valida_busca_por_identificador(bas)
-valida_acrescenta(bas)
-valida_atualiza(bas)
-# ======================================================================
-#! /usr/bin/python3
+# ----------------------------------------------------------------------
+sys.stderr.write("testando {compra.acrescenta}:\n")
 
-import tabela_de_sessoes
-import sessao; from sessao import ObjSessao
-import usuario; from usuario import ObjUsuario
-
-import tabela_de_itens_de_compras as tb_itens
-import base_sql
-import identificador
-import sys
-
-sys.stderr.write("Conectando com base de dados...\n")
-bas = base_sql.conecta("DB/MC857",None,None)
-
-sys.stderr.write("Criando tabela de itens de compras...\n")
-res = tb_itens.cria_tabela(bas)
-sys.stderr.write("Resultado = " + str(res) + "\n")
-
-#Teste acrescenta
-atrs = {
-  "id_compra" : "00000001",
-  "id_produto" : "00000002",
+cpr1_atrs = {
+  "id_produto" : "00000001",
   "qt" : "32",
   "preco" : "3.50"
 }
-#acrescenta linha
-tb_itens.acrescenta(bas,atrs)
-id_compra = int("00000001")
-lista = tb_itens.busca_por_compra(bas,id_compra)
-for i in lista:
-  sys.stderr.write(str(i[0]))
+cid1 = "C-00000001"
+cpr1 = testa_cria_compra("cpr1",cid1,cpr1_atrs)
 
+cpr2_atrs = {
+  "id_produto" : "00000002",
+  "qt" : "323",
+  "preco" : "7"
+}
+cid2 = "C-00000002"
+cpr2 = testa_cria_compra("cpr2",cid2,cpr2_atrs)
 
+cpr3_atrs = {
+  "id_produto" : "00000003",
+  "qt" : "342",
+  "preco" : "10.50"
+}
+cid3 = "C-00000003"
+cpr3 = testa_cria_compra("cpr3",cid3,cpr3_atrs)
 
-def valida_busca_por_indice(bas):
-    ses = sessao.cria(bas)
-    ind = tabela_de_sessoes.acrescenta(bas,ses)
-    res = tabela_de_sessoes.busca_por_indice(ind)
-    if ses != res:
-        print("O resultado esperado era " + str(ses) + " mas foi retornado " + str(res))
-    else:
-        print("Foi retornado o resultado esperado")
+# ----------------------------------------------------------------------
+sys.stderr.write("testando {compra.busca_por_compra}:\n")
+cpr1_c = compra.busca_por_compra(cid1)
+mostra_compra("cpr1_c",cpr1_c,cid1,cpr1_atrs)
 
-def valida_acrescenta(bas):
-    ultimo = bas.indice_inserido()
-    ses = sessao.cria(bas)
-    novo = tabela_de_sessoes.acrescenta(ses)
-    if novo <= ultimo:
-        print("O ID inserido esta incorreto")
-    else:
-        print("A entrada inserida está correta")
+# ----------------------------------------------------------------------
+sys.stderr.write("testando {usuario.busca_por_produto}:\n")
+cpr_id = cpr2_atrs["id_produto"]
+cpr2_p = compra.busca_por_produro(cpr_id)
+mostra_compra("cpr2_p",cpr2_p,cid2,cpr2_atrs)
 
-def valida_atualiza(bas):
-    ult_id = bas.indice_inserido()
-    ult_ses = tabela_de_sessoes.busca_por_indice(ult_id)
+cpr1_alts = {
+    "qt" : "2348",
+    "preco" : "3.50"
+    }
 
-    if ult_ses.aberta() == True:
-        ult_ses.logout()
-    else:
-        usr = usuario.cria(bas,atrs)
-        ult_ses.login(usr)
-    nova_aberta = ult_ses.aberta()
-    bas.atualiza(ult_ses)
+compra.muda_atributos(cpr1,cpr1_alts)
+cpr1_a = compra.busca_por_compra(cid1)
+cpr1_a_atrs
+for k,v in cpr1_atrs
+  cpr1_a_atrs[k]=v
+mostra_compra("cpr1_a",cpr1_a,cid1,cpr1_a_atrs)
 
-    res = bas.busca_por_indice(ult_id)
-    if res.aberta() != nova_aberta:
-        print("O resultado esta errado")
-    else:
-        print("O resultado está correto")
-
-
+if type(cpr2) is ObjCompra_IMP:
+    compra.muda_atributos(usr2,usr2_atrs)
+    mostra_compra("cpr2",cpr2,cid2,cpr2_atrs)
+    
+if type(cpr3) is ObjCompra_IMP:
+    compra.muda_atributos(usr3,usr2_atrs)
+    mostra_compra("cpr3",cpr3,cid2,cpr2_atrs)
