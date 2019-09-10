1ed1cd6503fdfa058811d048a206573560ebafa2
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-09-10 20:07:44 -0300

    Add interface e imp comando_subm_ver_produto

--- comando_subm_ver_produto_IMP.py
+++ comando_subm_ver_produto_IMP.py
@@ -2,13 +2,18 @@
 
 import gera_html_pag
 import produto
-import tabela_de_produtos
+import tabela_generica
 
 def processa(bas, sessao, args):
   id_produto = args['id_produto']
-  prod = tabela_de_produtos.busca_por_identificador(bas, id_produto)
+  # Considerando prod como objeto da classe Produto
+  prod = tabela_generica.busca_por_identificador(bas, id_produto)
+  atrs_produto = prod.obtem_atributos()
   if 'quantidade' in args:
     qt = float(args['quantidade'])
+    if qt > float(atrs_produto['quantidade']):
+        # Maior quantidade disponivel para o produto
+        qt = float(atrs_produto['quantidade'])
   else:
     qt = 1.0
   return gera_html_pag.mostra_produto(prod,qt)
