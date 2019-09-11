3715379435956671b50f800bf6522290be4e6e1c
guidumasperes <48294592+guidumasperes@users.noreply.github.com>
2019-09-10 21:06:37 -0300

    aprimoramento da interface e modulo comando_botao_sair.py e comando_botao_sair_IMP.py e prototipo do teste comando_botao_sair_TST.py

--- comando_botao_sair.py
+++ comando_botao_sair.py
@@ -1,8 +1,9 @@
 # Este módulo processa o acionamento do botão "Sair" do menu principal pelo usuário. 
 
 import comando_botao_sair_IMP
+import sessao
 
-def processa(bas,sessao):
+def processa(sessao):
   """Encerra a {sessao} dada e retorna o HTML da página de entrada"""
-  return comando_botao_sair_IMP.processa(bas,sessao)
+  return comando_botao_sair_IMP.processa(sessao)
 
--- comando_botao_sair_IMP.py
+++ comando_botao_sair_IMP.py
@@ -2,8 +2,9 @@
 
 import gera_html_pag
 
-def processa(bas,sessao):
-  # !!! Completar !!!
-  assert False
+def processa(sessao):
+  do {
+      objeto_sessao = sessao.logout()
+  while(sessao.aberta())
   return gera_html_pag.entrada()
     
