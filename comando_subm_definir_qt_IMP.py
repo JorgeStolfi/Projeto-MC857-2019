53ab4a03c63cc759ef8fcff04e007e7395f0d036
Vinícius Pedroso <vpedroso.ee@gmail.com>
2019-09-10 21:39:52 -0300

    Corrigir verificacao quantidade args e arg_produto

--- comando_subm_definir_qt_IMP.py
+++ comando_subm_definir_qt_IMP.py
@@ -1,14 +1,17 @@
-# Implementação do módulo {comando_subm_definir_qt}.
+# Implementação do módulo {comando_subm_ver_produto}.
 
 import gera_html_pag
 import produto
-import tabela_de_produtos
+import tabela_generica
 
 def processa(bas, sessao, args):
   id_produto = args['id_produto']
-  prod = tabela_de_produtos.busca_por_identificador(bas, id_produto)
-  if 'quantidade' in args:
-    qt = float(args['quantidade'])
+  # Considerando prod como objeto da classe Produto
+  prod = tabela_generica.busca_por_identificador(bas, id_produto)
+  atrs_produto = prod.obtem_atributos()
+  if ('quantidade' in args) and ('quantidade' in atrs_produto):
+    # No maximo a maior quantidade disponivel para o produto
+    qt = min(float(args['quantidade']), float(atrs_produto['quantidade']))
   else:
     qt = 1.0
   return gera_html_pag.mostra_produto(prod,qt)
