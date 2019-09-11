ab86fb42d7aee3d27a0797e17b991528101e26dd
ericoimf <ericoimf@gmail.com>
2019-09-10 21:58:41 -0300

    Update sessao_IMP.py

--- sessao_IMP.py
+++ sessao_IMP.py
@@ -1,54 +1,43 @@
-# Imprementação da classe {Sessao}.
+import tabela_generica as tabela
 
-import tabela_generica
+def inicializa():
+    tabela.cria_tabela("sessoes", ["id_sessao", "usr", "aberta"])
 
-class Sessao_IMP:
-
-    def __init__(self, bas, usr):
+class ObjSessao_IMP:
+    def __init__(self, usr):
         self.id_sessao = None
         self.usr = usr
-        self.bas = bas
-        self.colunas = (
-            ('id_usuario', 'char(10)'),
-            ('aberta', 'boolean')
-        )
-        campos = "indice integer NOT NULL PRIMARY KEY"
-        for c in self.colunas:
-            campos = campos + ", " + c[0] + " " + c[1]
-        self.bas.executa_comando_CREATE_TABLE("sessoes", campos);
+        self.aberta = True
 
-    def busca_por_identificador(self, id_sessao):
-        cmd = "SELECT * FROM sessoes WHERE id = " + id_sessao
-        sessao = self.bas.executa_comando_SELECT("sessoes", cmd)
-        return sessao
+def cria(usr):
+    sessao = ObjSessao_IMP(usr)
+    tabela.atualiza("sessoes", sessao)
+    return sessao
 
-    def aberta(self):
-        return self.usr is not None
+def obtem_identificador(sessao):
+    return sessao.id_sessao
 
-    def obtem_usuario(self):
-        return self.usr
+def obtem_usuario(sessao):
+    return sessao.usr
 
-    def atualiza(self, ind, ses):
-        usr = ses.obtem_usuario()
-        uid = (usr.obtem_identificador() if usr != None else None)
-        pares = \
-            "usr=" + str(uid) + ", " + \
-            "sessao_aberta=" + str(ses.aberta())
-        cmd = "UPDATE sessoes SET " + pares + " WHERE id=" + str(ind)
-        self.bas.executa_comando_UPDATE("sessoes", )
+def busca_por_identificador(id_sessao):
+    sessao = tabela.busca_por_identificador("sessoes", id_sessao)
+    return sessao
 
-    def acrescenta(self, ses):
-        id = bas.executa_comando_INSERT("sessoes", self)
-        return id
+def aberta(sessao):
+    return sessao.aberta
 
-    def logout(self, bas):
-        self.usr = None
-        tabela_de_sessoes.atualiza(bas, self.id_sessao, self)
-        
+#obtem_atributos
 
-def cria(bas,usr):
-    ses = Sessao_IMP()
-    ids = ses.acrescenta(bas)
-    ses.usr = usr
-    ses.id_sessao = ids
-    return ses
+def muda_atributos(sessao, alts):
+    sessao.usr = alts.usr
+    sessao.aberta = alts.aberta
+    sessao.id_sessao = alts.id_sessao
+    tabela.atualiza("sessoes", sessao)
+
+
+def logout(sessao):
+    sessao.usr = None
+    tabela.atualiza("sessoes", sessao)
+
+#campos
