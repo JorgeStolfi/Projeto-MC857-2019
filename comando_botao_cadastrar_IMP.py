932eb7495b4a6aaa2a51cea5f0a6cb6f72678425
ericoimf <ericoimf@gmail.com>
2019-09-17 20:30:30 -0300

    Update comando_botao_cadastrar_IMP.py

--- comando_botao_cadastrar_IMP.py
+++ comando_botao_cadastrar_IMP.py
@@ -1,6 +1,9 @@
 # Implementação do módulo {comando_botao_cadastrar}.
 
-import gera_html_pag
+import gera_html_form
+import tabela_generica
 
 def processa():
-  return gera_html_pag.cadastrar_usuario()
+  conteudo_cadastro = gera_html_form.cadastrar_usuario()
+  cadastro = tabela_generica.acrescenta("usuarios", conteudo_cadastro)
+  return cadastro
