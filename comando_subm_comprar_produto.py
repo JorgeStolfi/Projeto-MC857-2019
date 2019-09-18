87393499111aa23bd920c90ad750b6cd073e3eb7
João Victor Fernandes <lfvictorf@gmail.com>
2019-09-17 21:05:20 -0300

    Implementacao do arquivo comando_subm_comprar_produto. Feito por RA 158043 - João Victor A. Fernandes

--- comando_subm_comprar_produto.py
+++ comando_subm_comprar_produto.py
@@ -1,10 +1,10 @@
-# Este módulo processa o acionamento pelo usuário do botão "Comprar" numa página 
+# Este módulo processa o acionamento pelo usuário do botão "Comprar" numa página
 # de um produto, depois de preencher a quantidade desejada.
 
 import comando_subm_comprar_produto_IMP
 
-def processa(bas, sessao, args):
-    """Espera o identificador do produto "P-{NNNNNNNN}" em {args['id_produto']} e a quantidade
-    desejada em {args['quantidade']} (na forma de um string).
+def processa(sessao, dados):
+    """Espera o identificador do produto "P-{NNNNNNNN}" em {dados['id_produto']} e a quantidade
+    desejada em {dados['quantidade']} (na forma de um string).
     Acrescenta esse item ao carrinho de compras da {sessao} dada."""
-    return comando_subm_comprar_produto_IMP.processa(bas, sessao, args)
+    return comando_subm_comprar_produto_IMP.processa(sessao, dados)
--- comando_subm_comprar_produto_IMP.py
+++ comando_subm_comprar_produto_IMP.py
@@ -5,10 +5,10 @@ import compra
 import produto
 import tabela_de_produtos
 
-def processa(bas, sessao, args):
-  id_produto = args['id_produto']
+def processa(sessao, dados):
+  id_produto = dados['id_produto']
   prod = tabela_de_produtos.busca_por_identificador(bas, id_produto)
-  quantidade = float(args['quantidade'])
+  quantidade = float(dados['quantidade'])
   carrinho = sessao.obtem_carrinho()
-  carrinho.acrescenta_item(bas, prod, quantidade)
+  carrinho.acrescenta_item(prod, quantidade)
   return gera_html_pag.lista_compra(carrinho)
