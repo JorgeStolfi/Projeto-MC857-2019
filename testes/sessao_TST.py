7295c3c60692b4ff3d74b03bb1900a8133966f76
qiavegatto <s.l.chiavegatto@gmail.com>
2019-09-24 20:34:30 -0300

    Update sessao_TST.py

--- testes/sessao_TST.py
+++ testes/sessao_TST.py
@@ -7,6 +7,7 @@ import usuario
 import identificador
 import utils_testes
 import sys
+import compra
 
 # ----------------------------------------------------------------------
 sys.stderr.write("Conectando com base de dados...\n")
@@ -15,6 +16,9 @@ base_sql.conecta("DB/MC857",None,None)
 # ----------------------------------------------------------------------
 sys.stderr.write("Inicializando módulo {usuario}, limpando tabela, criando usuários para teste:\n")
 usuario.cria_testes()
+
+sys.stderr.write("Inicializando módulo {compra}, limpando tabela, criando compras para teste:\n")
+compra.cria_testes()
 
 sys.stderr.write("Inicializando módulo {sessao}, limpando tabela:\n")
 sessao.inicializa(True)
@@ -25,16 +29,19 @@ sys.stderr.write("Obtendo dois usuários para teste:\n")
 usr1 = usuario.busca_por_identificador("U-00000001")
 usr2 = usuario.busca_por_identificador("U-00000002")
 
+cmp1 = compra.busca_por_identificador("C-00000001")
+cmp2 = compra.busca_por_identificador("C-00000003")
+
 # ----------------------------------------------------------------------
 # Funções de teste:
 
 ok_global = True # Vira {False} se um teste falha.
 
-def verifica_sessao(rotulo, ses, indice, ident, usr, abrt, cookie):
+def verifica_sessao(rotulo, ses, indice, ident, usr, abrt, cookie, carrinho):
   """Testes básicos de consistência do objeto {ses} da classe {ObjSessao}, dados {indice},
   {ident} e {atrs} esperados."""
   global ok_global
-  atrs = { 'usr': usr, 'abrt': abrt, 'cookie': cookie }
+  atrs = { 'usr': usr, 'abrt': abrt, 'cookie': cookie, 'carrinho': carrinho }
   ok = utils_testes.verifica_objeto(rotulo, sessao, sessao.ObjSessao, ses, indice, ident, atrs)
   
   if ses != None and type(ses) is sessao.ObjSessao:
@@ -56,6 +63,12 @@ def verifica_sessao(rotulo, ses, indice, ident, usr, abrt, cookie):
     if cookie1 != cookie:
       sys.stderr.write("  **erro: retornou " + str(cookie1) + ", deveria ter retornado " + str(cookie) + "\n")
       ok = False
+      
+    sys.stderr.write("testando {obtem_carrinho()}:\n")
+    carrinho1 = sessao.obtem_carrinho(ses)
+    if carrinho1 != carrinho:
+      sys.stderr.write("  **erro: retornou " + str(carrinho1) + ", deveria ter retornado " + str(carrinho) + "\n")
+      ok = False
  
   ok_global = ok_global and ok
   return
@@ -63,25 +76,25 @@ def verifica_sessao(rotulo, ses, indice, ident, usr, abrt, cookie):
 # ----------------------------------------------------------------------
 sys.stderr.write("testando {sessao.cria}:\n")
 scook1 = "ABCDEFGHIJK"
-s1 = sessao.cria(usr1, scook1)
+s1 = sessao.cria(usr1, scook1, cmp1)
 sindice1 = 1
 sident1 = "S-00000001"
-verifica_sessao("s1", s1, sindice1, sident1, usr1, True, scook1)
+verifica_sessao("s1", s1, sindice1, sident1, usr1, True, scook1, cmp1)
 
 scook2 = "BCDEFGHIJKL"
-s2 = sessao.cria(usr2, scook2)
+s2 = sessao.cria(usr2, scook2, cmp2)
 sindice2 = 2
 sident2 = "S-00000002"
-verifica_sessao("s2", s2, sindice2, sident2, usr2, True, scook2)
+verifica_sessao("s2", s2, sindice2, sident2, usr2, True, scook2, cmp2)
 
 scook3 = "CDEFGHIJKLM"
-s3 = sessao.cria(usr1, scook3)
+s3 = sessao.cria(usr1, scook3, cmp1)
 sindice3 = 3
 sident3 = "S-00000003"
-verifica_sessao("s3", s3, sindice3, sident3, usr1, True, scook3)
+verifica_sessao("s3", s3, sindice3, sident3, usr1, True, scook3, cmp1)
 
 sessao.fecha(s1)
-verifica_sessao("fecha s1", s1, sindice1, sident1, usr1, False, scook1)
+verifica_sessao("fecha s1", s1, sindice1, sident1, usr1, False, scook1, cmp1)
 
 # ----------------------------------------------------------------------
 # Veredito final:
