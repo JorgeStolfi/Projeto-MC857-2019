298d7396ddd69efc7f882300f999ea4138ce5948
Vinícius Balbino <vbalbinods@gmail.com>
2019-09-24 20:01:50 -0300

    Add files via upload

---
+++ comando_subm_finalizar_compra.py
@@ -0,0 +1,8 @@
+# Este módulo processa a acao do usuario que consiste em pedir para ver a lista de todos os 
+# pedidos de compras do usuário, em qualquer estado.
+
+import comando_subm_finalizar_compra
+
+def processa(ses, args):
+return comando_subm_ver_compras_IMP.processa(ses,args)
+    
