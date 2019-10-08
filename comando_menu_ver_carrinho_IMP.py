22e0a57b2d582206039ef82ece80b9518e1f890c
Gabriel Alves Tabchoury <ra171828@oliveira.lab.ic.unicamp.br>
2019-10-08 19:26:23 -0300

    comentando melhor trecho do codigo

--- comando_menu_ver_carrinho_IMP.py
+++ comando_menu_ver_carrinho_IMP.py
@@ -5,5 +5,6 @@ import sessao
 from utils_testes import erro_prog, mostra
 
 def processa(ses, args):
-  pag = gera_html_pag.mostra_carrinho(ses)
-  return pag
+  # Chama a funcao que gera a pagina HTML do carrinho a partir da sessao do usuario
+  pagina_carrinho = gera_html_pag.mostra_carrinho(ses)
+  return pagina_carrinho
