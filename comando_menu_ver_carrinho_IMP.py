f78b8a0528429000dc333d9ba9dd43385a72bdc0
Gabriel Alves Tabchoury <ra171828@oliveira.lab.ic.unicamp.br>
2019-10-08 20:06:46 -0300

    campo de quantidade editavel e botao de remover

--- comando_menu_ver_carrinho_IMP.py
+++ comando_menu_ver_carrinho_IMP.py
@@ -15,11 +15,17 @@ def processa(ses, args):
   compra = sessao.obtem_carrinho(ses)
 
   pagina_carrinho = "<html><table><tbody>"
-  pagina_carrinho = pagina_carrinho + "<tr><td>Produto</td><td>Quantidade</td><td>Preço</td></tr>"
+  pagina_carrinho = pagina_carrinho + "<tr><td><b>Produto</b></td> \
+  <td><b>Quantidade</b></td> \
+  <td><b>Preço</b></td> \
+  <td><b>Excluir</b></td></tr>"
   for prod, qt, prc in compra.itens:
     atrs = produto.obtem_atributos(prod)
     d_curta = atrs['descr_curta']
-    pagina_carrinho = pagina_carrinho + "<tr><td>"+d_curta+"</td><td>"+str(qt)+"</td><td>"+str(prc)+"</td></tr>"
+    pagina_carrinho = pagina_carrinho + "<tr><td>"+d_curta+"</td> \
+    <td><input type=\"number\" value=\""+str(qt)+"\"></td> \
+    <td>R$ "+str(prc)+"</td> \
+    <td><input type=\"button\" value=\"Remover\"></td></tr>"
 
   pagina_carrinho = pagina_carrinho + "</tbody></table></html>"
   
