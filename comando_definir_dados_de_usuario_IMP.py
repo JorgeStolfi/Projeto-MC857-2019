9552621ba18050ca5de733cdfe28a1682b3fa1a3
Guilherme Domingues <guilhermeluisdomingues@gmail.com>
2019-10-29 20:15:10 -0300

    reduzindo tamanho do endereco

--- comando_definir_dados_de_usuario_IMP.py
+++ comando_definir_dados_de_usuario_IMP.py
@@ -42,8 +42,8 @@ def processa(ses, args):
     erro = 'O tamanho do nome deve ser entre 1 e 60 caracteres'  
   elif len(args['email']) < 6 or len(args['email']) > 60:
     erro = 'O tamanho do email deve ser entre 6 e 60 caracteres'  
-  elif len(args['endereco']) < 30 or len(args['endereco']) > 180:
-    erro = 'O tamanho do endereco deve ser entre 30 e 180 caracteres'  
+  elif len(args['endereco']) < 10 or len(args['endereco']) > 180:
+    erro = 'O tamanho do endereco deve ser entre 10 e 180 caracteres'  
   elif len(args['documento']) < 6 or len(args['documento']) > 24:
     erro = 'O tamanho do documento deve ser entre 6 e 24 caracteres'  
   elif len(args['telefone']) < 9 or len(args['telefone']) > 40:
